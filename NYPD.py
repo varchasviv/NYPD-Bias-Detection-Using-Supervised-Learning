14518ecc5207aa76a34c2b22e1541cdd138f58ed
AVIJIT NALWA <NALWA.AVIJIT@GMAIL.COM>
2020-03-27 18:00:26 -0700

    Adding models
    adding trained and saved random forest models for each level of Perp_Race

--- NYPD.py
+++ NYPD.py
@@ -14,44 +14,37 @@ nypd = pd.read_pickle('nypd_df.pkl')
 nypd = nypd[['PD_DESC', 'PERP_RACE', 'LAW_CODE', 'AGE_GROUP', 'ARREST_BORO', 'PERP_SEX', 'LAW_CAT_CD']]
 nypd.dropna(inplace=True)
 
-#print(nypd.head())
+races = nypd['PERP_RACE'].unique()
 
-black = nypd.loc[nypd['PERP_RACE'] == "BLACK"]
-print(black.head())
-black.drop(['PERP_RACE'], axis=1, inplace=True)
+for race in races:
+    df_race = nypd.loc[nypd['PERP_RACE'] == race]
+    df_race.drop(['PERP_RACE'], axis=1, inplace=True)
 
-train, test = train_test_split(black, test_size=0.2)
+    train, test = train_test_split(df_race, test_size=0.2)
 
-predictors = train.columns.values[:-1]
-target = train.columns.values[-1]
+    predictors = train.columns.values[:-1]
+    target = train.columns.values[-1]
 
-print(predictors)
-print(target)
+    model = H2ORandomForestEstimator(
+        model_id="rf_"+race,
+        ntrees=200,
+        stopping_rounds=7,
+        score_each_iteration=True,
+        max_depth=50,
+        seed=1000000)
 
-rf_v1 = H2ORandomForestEstimator(
-    model_id="rf_covType_v1",
-    ntrees=200,
-    stopping_rounds=7,
-    score_each_iteration=True,
-    max_depth=50,
-    seed=1000000)
+    h_train = h2o.H2OFrame(train)
+    h_test = h2o.H2OFrame(test)
 
-h_train = h2o.H2OFrame(train)
-h_test = h2o.H2OFrame(test)
+    model.train(list(predictors), str(target), training_frame=h_train, validation_frame=h_test)
+    print(model)
 
-rf_v1.train(list(predictors), str(target), training_frame=h_train, validation_frame=h_test)
+    h2o.save_model(model=model, path="rf_"+race, force=True)
+    print(model.score_history())
 
-print(rf_v1)
-
-
-# To look at validation statistics, we can use the scoring history function.
-
-# In[ ]:
-
-print(rf_v1.score_history())
-
+# for reference:
 # save the model
-h2o.save_model(model=rf_v1, path="rf_v1", force=True)
+# h2o.save_model(model=rf_v1, path="rf_v1", force=True)
 
 # print model_path
 # /tmp/mymodel/DeepLearning_model_python_1441838096933
